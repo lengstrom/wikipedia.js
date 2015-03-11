a2b918107e69110d7042fffde12ded0512d866ff
Logan Engstrom <meadowstream@users.noreply.github.com>
2015-03-11 09:40:09 -0400

    added modules to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
-var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+
 gulp.task('build', function() {
 	gulp.task('js', function () {
 		return gulp.src('js/*.js')
