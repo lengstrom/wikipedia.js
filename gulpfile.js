3ea346e118f76e3ff546630104bc591bb0aa7e05
Logan Engstrom <meadowstream@users.noreply.github.com>
2015-03-11 12:48:14 -0400

    added to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,10 @@ var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 
 gulp.task('build', function() {
-	gulp.task('js', function () {
-		return gulp.src('js/*.js')
-		.pipe(jshint())
-		.pipe(jshint.reporter('default'))
-		.pipe(uglify())
-		.pipe(concat('app.js'))
-		.pipe(gulp.dest('build'));
-	});
+	return gulp.src('js/*.js')
+	.pipe(jshint())
+	.pipe(jshint.reporter('default'))
+	// .pipe(uglify())
+	.pipe(concat('app.js'))
+	.pipe(gulp.dest('build'));
 });
